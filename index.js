9d8d6a512271cb41e3ed6f93cec7a0af37d1177d
utp0 <paksyh15@gmail.com>
2024-04-12 18:55:30 +0200

    req.body parser uncomment, register form mezőnevek

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 function isAdmin(req) { return req.session.jogusultsag >= 2; }
 function isUser(req)  { return req.session.jogusultsag >= 1; }
